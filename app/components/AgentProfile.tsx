1b964025c5636d19b23e0f5dd37f4496aad6e401
Alissa Crane <alissa.crane@coinbase.com>
2024-11-05 15:31:45 -0800

    fix imports

--- app/components/AgentProfile.tsx
+++ app/components/AgentProfile.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import type { Language } from '../types';
 import { AGENT_NAME, AGENT_WALLET_ADDRESS, notoSansThai } from '../constants';
 import { translations } from '../translations';
+import type { Language } from '../types';
 
 type AgentProfileProps = {
   currentLanguage: Language;
