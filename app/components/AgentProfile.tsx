191fbc7eb941f5b328af29693203ca1a1955ef21
Alissa Crane <alissa.crane@coinbase.com>
2024-11-06 15:59:24 -0800

    fix build

--- app/components/AgentProfile.tsx
+++ app/components/AgentProfile.tsx
@@ -104,11 +104,11 @@ export default function AgentProfile({ currentLanguage }: AgentProfileProps) {
           }`}
         >
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
-      </div>
+      </div>b
     </div>
   );
 }
