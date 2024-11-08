a709b6c2605fa33eea718828f0cd890ae3b9ff5a
Alissa Crane <alissa.crane@coinbase.com>
2024-11-08 10:21:35 -0800

    fix format

--- app/components/StreamItem.tsx
+++ app/components/StreamItem.tsx
@@ -6,11 +6,12 @@ type StreamItemProps = {
 };
 
 export default function StreamItem({ entry }: StreamItemProps) {
-
   return (
     <div className="mb-2">
       <TimeDisplay timestamp={entry.timestamp} />
-      <div className={`flex items-center space-x-2 max-w-full ${entry?.type !== 'user' ? 'text-[#5788FA]' : 'text-gray-300'}`}>
+      <div
+        className={`flex items-center space-x-2 max-w-full ${entry?.type !== 'user' ? 'text-[#5788FA]' : 'text-gray-300'}`}
+      >
         <span className="max-w-full text-wrap break-all"> {entry.content}</span>
       </div>
     </div>
